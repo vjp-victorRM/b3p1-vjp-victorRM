81e000cf4e47f821a8704bf7d972234ce035b255
vruanom01 <vruanom01@informatica.iesvalledeljerteplasencia.es>
2022-10-17 20:54:11 +0200

    17 - 10

---
+++ Pruebas/Js/Ejercicio6.js
@@ -0,0 +1,29 @@
+function creaMatriz(){
+    let contenedor = document.querySelector("#contenedorGeneral");
+    for (var i = 0; i < 10; i++){
+        let saltodeLinea = document.createElement("br");
+        contenedor.appendChild(saltodeLinea);
+        for (var j = 0; j < 10; j++){
+            let filas = document.createElement("button");
+            contenedor.appendChild(filas);
+            filas.style.width = "25px";
+            filas.style.height = "25px";
+            filas.style.margin = "10px";
+        }
+
+    }
+
+}
+creaMatriz();
+
+function asignaColores(){
+    let colores = ["red","blue","green","yellow"];
+    switch (colores){
+        case 1:
+            if(button.event = 0) (
+
+            )
+    }
+}
+
+
