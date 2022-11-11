d9c255853dd6e752ad6804e547948866d3bc8098
vruanom01 <vruanom01@informatica.iesvalledeljerteplasencia.es>
2022-11-11 19:25:29 +0100

    09/11

--- UT4/P1/JS/Ejercicio3_Promises.js
+++ UT4/P1/JS/Ejercicio3_Promises.js
@@ -1,8 +1,8 @@
-
-let boton = document.addEventListener("DOMContentLoaded",function (){
-    boton.addEventListener("click",pintarMuseo())
+let boton = document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("boton").addEventListener("click", cargarMuseos());
 });
-function peticion() {
+
+function cargarMuseos() {
     let peticionAjax = new XMLHttpRequest();
     peticionAjax.addEventListener("readystatechange", procesarPeticion);
     peticionAjax.open("GET", "https://raw.githubusercontent.com/fsangar/backupOpendataCCJSON/master/museos.json");
@@ -10,26 +10,37 @@ function peticion() {
 }
 
 
-function procesarPeticion(event){
-    if(this.readyState == 4 && this.status == 200){
-        let objetoResultado = JSON.parse(this.responseText);
-        procesarResultado(objetoResultado);
+function procesarPeticion(event) {
+    if (this.readyState == 4 && this.status == 200) {
+        let objetoMuseo = JSON.parse(this.responseText);
+        procesarResultado(objetoMuseo);
     }
 }
 
-function pintarMuseo(objetoMuseo){
-    let contenendor = document.getElementById('contenedorTabla')
+function pintarMuseo(objetoMuseo) {
+    let contenendor = document.getElementById('contenedorTabla');
     contenendor.innerHTML =
         <tr>Nombre</tr>
-        <tr>Latitud</tr>
-        <tr>Longitud</tr>
-        <tr>Url Web</tr>;
+    <tr>Latitud</tr>
+    <tr>Longitud</tr>
+    <tr>Url Web</tr>;
     let tabla = document.createElement("tr");
     tabla.innerHTML =
-    <td>objetoMuseo.rdfs_label.value</td>
+        <td>objetoMuseo.rdfs_label.value</td>
     <td>objetoMuseo.geo_lat.value</td>
     <td>objetoMuseo.geo_long.value</td>
     <td>objetoMuseo.uri.value</td>;
 }
+
+function procesarResultado(objetoMuseo) {
+    let cabecera = document.getElementById("contenedorTabla")
+    cabecera.innerHTML = <tr>Nombre</tr>, <tr>Latitud</tr>, <tr>Longitud</tr>, <tr>Url Web</tr>
+    for (let museo of objetoMuseo.results.bindings) {
+        console.log(pintarMuseo(objetoMuseo));
+    }
+}
+
+
+
 
 
